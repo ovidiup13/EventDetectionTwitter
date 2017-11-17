a7af5bc483d0867181da3ad1675c5c558934cbd1
Ovidiu Popoviciu <devovip13@gmail.com>
2017-11-17 15:51:40 +0000

    Rename scripts folder to public

--- public/src/index.js
+++ public/src/index.js
@@ -5,14 +5,13 @@ const evaluation = require("./eval");
 const parse = require("./parse");
 
 const inputPath = process.argv[2];
-
 const outputFile = process.argv[3];
-const outputPath = "../public/assets";
 
 fs.readdir(inputPath, (err, items) => {
+    console.log(items);
     runEval(items).then(data => {
         data.sort((d1, d2) => parseInt(d1.file) - parseInt(d2.file));
-        fs.writeFile(path.join(outputPath, outputFile), JSON.stringify(data), err => {
+        fs.writeFile(outputFile, JSON.stringify(data), err => {
             if (err) {
                 console.error(err);
             }
